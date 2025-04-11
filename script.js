868aa1ae902f89f237da5d337c33c70f168d2106
annylinda0 <143603431+annylinda0@users.noreply.github.com>
2025-04-11 01:48:45 +0000

    aula 6

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function () {
+    
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       });
+    
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    
     let tamanhoAtualFonte = 1;
+    
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -14,11 +25,6 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
-
 });
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-botaoDeAcessibilidade.addEventListener('click', function (){
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
+   
+    
